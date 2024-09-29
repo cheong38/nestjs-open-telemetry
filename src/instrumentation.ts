c03f85cddcc84b60f045446bcc464bb0fd457fb0
Woojin Jung <cheong0308@gmail.com>
2024-09-30 02:31:51 +0900

    Remove metric related console logging

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,19 +1,12 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { InMemorySpanExporter } from "@opentelemetry/sdk-trace-node";
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} from '@opentelemetry/sdk-metrics';
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import {
   ConsoleLogRecordExporter,
   LoggerProvider,
   SimpleLogRecordProcessor
 } from "@opentelemetry/sdk-logs";
-import { logs } from "@opentelemetry/api-logs";
-
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+import { logs, SeverityNumber } from "@opentelemetry/api-logs";
 
 const loggerProvider = new LoggerProvider()
 loggerProvider.addLogRecordProcessor(
@@ -23,12 +16,12 @@ loggerProvider.addLogRecordProcessor(
 logs.setGlobalLoggerProvider(loggerProvider)
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new InMemorySpanExporter(),
   // NOTE: 현재 프로젝트의 실험 범위에서 metric 관련 Exporting 은 대상이 아니어서 주석 처리
   // metricReader: new PeriodicExportingMetricReader({
   //   exporter: new ConsoleMetricExporter(),
   // }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
-
+SeverityNumber
 sdk.start();
